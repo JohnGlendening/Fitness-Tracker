8b81cc27b39278dc08bd92e946a620df4f3c8040
John Glendening <66345555+JohnGlendening@users.noreply.github.com>
2020-10-08 20:19:54 -0500

    Add files via upload

--- orm.js
+++ orm.js
@@ -7,17 +7,17 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 const Schema = mongoose.Schema;
 
 const workOutSchema = new Schema({
-    type: String,
-    name: String,
-    duration: Number,
-    weight: Number,
-    reps: Number,
-    sets: Number,
-    distance: Number,
-    date: { type: Date, default: Date.now },
-    completed: { type: Boolean, default: false },
-    updated_at: { type: Date, default: Date.now }
-})
+            type: String,
+            name: String,
+            duration: Number,
+            weight: Number,
+            reps: Number,
+            sets: Number,
+            distance: Number,
+            date: { type: Date, default: Date.now },
+            completed: { type: Boolean, default: false },
+            updated_at: { type: Date, default: Date.now }
+    })
 const WorkOut = mongoose.model("WorkOut", workOutSchema);
 
 const workOutDaySchema = new Schema({
@@ -26,81 +26,79 @@ const workOutDaySchema = new Schema({
 })
 const WorkOutDay = mongoose.model("WorkOutDay", workOutDaySchema);
 //======
-async function createWorkout(myPost) {
+async function createWorkout( myPost ){
     let workOutDay;
     const currentDay = new Date();
     // workOutDay = await WorkOutDay.create( { date: currentDay} );
     // const workOutDayId = workOutDay._id;
     let workoutType = myPost.type;
-    if (workoutType === "resistance") {
-        const myResult = await WorkOut.create({
-            type: myPost.type,
-            name: myPost.name,
-            duration: myPost.duration,
-            weight: myPost.weight,
-            reps: myPost.reps,
-            sets: myPost.sets
-        });
-        console.log('in orm created workout is ', myResult)
-            // const wrkOutId = myResult._id;
-            // dbResult = await WorkOutDay.update({ date: currentDay}, { $push: { workouts: wrkOutId } });
-            // dbResult = await WorkOutDay.find({ date: currentDay});
-            // console.log(`current day data is :`, dbResult)
+    if (workoutType === "resistance"){
+        const myResult = await WorkOut.create( { 
+                type: myPost.type,
+                name: myPost.name,
+                duration: myPost.duration,
+                weight: myPost.weight,
+                reps:  myPost.reps,
+                sets: myPost.sets
+        } );
+        console.log( 'in orm created workout is ', myResult)
+        // const wrkOutId = myResult._id;
+        // dbResult = await WorkOutDay.update({ date: currentDay}, { $push: { workouts: wrkOutId } });
+        // dbResult = await WorkOutDay.find({ date: currentDay});
+        // console.log(`current day data is :`, dbResult)
 
         return myResult;
 
     } else if (workoutType === "cardio") {
-        const myResult = await WorkOut.create({
+        const myResult = await WorkOut.create( { 
             type: myPost.type,
             name: myPost.name,
             distance: myPost.distance,
             duration: myPost.duration
-        });
-        console.log('in orm created workout is ', myResult)
+        } );
+        console.log( 'in orm created workout is ', myResult)
         return myResult;
     }
 }
 
-async function listWorkOuts() {
-    const myList = await WorkOut.find({ completed: false })
+async function listWorkOuts(){
+    const myList =  await WorkOut.find({ completed: false})
     console.log('my list in orm is : ', myList);
     return myList;
 }
-async function listCompletedWorkOuts() {
-    const myList = await WorkOut.find({ completed: true })
+async function listCompletedWorkOuts(){
+    const myList =  await WorkOut.find({ completed: true})
     console.log('my completed last workout in orm is : ', myList);
     return myList;
 }
-async function listNonCompletedWorkOuts() {
-    const myNotCompletedList = await WorkOut.find({ completed: false })
-        // console.log('my not completed last workout in orm is : ', myNotCompletedList);
+async function listNonCompletedWorkOuts(){
+    const myNotCompletedList =  await WorkOut.find({ completed: false})
+    // console.log('my not completed last workout in orm is : ', myNotCompletedList);
     return myNotCompletedList;
 }
-async function lastWorkOut() {
-    const myLastWrkOutDay = await WorkOut.find({ completed: true, }).sort({ _id: -1 }).limit(1)
-    const lastDate = myLastWrkOutDay[0].date
+async function lastWorkOut(){
+    const myLastWrkOutDay =  await WorkOut.find({ completed: true, }).sort({ _id: -1 }).limit(1)
+    const lastDate= myLastWrkOutDay[0].date
     console.log('my last workout date in orm is : ', lastDate);
     // console.log('my completed last workout in orm is : ', myLastWrkOut);
-
-    const myLastWrkOuts = await WorkOut.find({ date: lastDate });
+    
+    const myLastWrkOuts =  await WorkOut.find({ date: lastDate});
 
     return myLastWrkOuts;
 }
 
 // completeWorkout
-async function completeWorkout(workOtId, date) {
+async function completeWorkout( workOtId, date ){
 
-    const myResult = await WorkOut.update({ _id: workOtId }, {
-        $set: {
-            completed: true,
-            date: date
-        }
-    })
+    const myResult = await WorkOut.update({_id: workOtId},
+        { $set:{    completed: true,
+                    date: date
+        } })
     console.log(myResult);
     return myResult;
 }
 
-module.exports = {
+module.exports = { 
     createWorkout,
     listWorkOuts,
     completeWorkout,
--- public/index.js
+++ public/index.js
@@ -2,12 +2,16 @@ init();
 
 async function init() {
   if (location.search.split("=")[1] === undefined) {
-    const workout = await API.getLastWorkout();
-    if (workout) {
-      location.search = "?id=" + workout._id;
-    } else {
-      document.querySelector("#continue-btn").classList.add("d-none")
-    }
+    // const workout = await API.getLastWorkout();
+    // const workout = await $.get( '/api/lastworkouts');
+    // const workout2 = await $.get( '/api/uncompletedworkouts');
+    // console.log('my uncompleted workout is ', workout2)
+
+    // if (workout2 = null ) {
+    //   // $('#cntBtn')
+    //   document.querySelector("#cntBtn-btn").classList.add("d-none")
+    // }
+    
   }
 }
 
